64ded71ffa5923098cfd7303c886f3e0feba1611
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-01-11 20:52:32 +0000

    Attempted fixing 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,37 +1,16 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static.
 # Installation of nginx if not already installed
-if ! command -v nginx &> /dev/null
-then
-    apt-get update
-    apt-get install -y nginx
-fi
-
+apt-get update
+apt-get upgrade
+apt-get install -y nginx
 #folder creations
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>
-ubuntu@89-web-01:~/$ curl localhost/hbnb_static/index.html
-<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | tee /data/web_static/releases/test/index.html > /dev/null 2>&1
-
+mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+echo "Hello Again World!" | tee /data/web_static/releases/test/index.html > /dev/null 2>&1
 # Symbolic link
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
 # Ownership
 chown -hR ubuntu:ubuntu /data/
-
 # creating /hbnb_static location
 nginx_conf="/etc/nginx/sites-available/default"
 content_to_insert="\\
@@ -41,5 +20,4 @@ if ! grep -qF "location /hbnb_static" "$nginx_conf"
 then
     sed -i "/error_page 404 \/404.html;/a $content_to_insert" "$nginx_conf"
 fi
-
-service nginx restart
+service nginx start
