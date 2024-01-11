61b97662bc8efc2c274f88befef28470bf8b6265
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-01-11 15:38:38 +0000

    Added restart

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,40 +11,27 @@ service nginx start
 #folder creations
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-body {
-  margin: 0;
-  background: #5d9ab2;
-}
-.center_div {
-  border: 1px solid gray;
-  margin-left: auto;
-  margin-right: auto;
-  width: 100%;
-  background-color: #d0f0f6;
-  text-align: left;
-  padding: 5px;
-}
-</style>
-</head>
-<body>
-<div class='center_div'>
-  <h1>Hello World!</h1>
-  <p>This example contains some advanced CSS methods you may not have learned yet. But, we will explain these methods in a later chapter in the tutorial.</p>
-</div>
-</body>
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
 </html>
-" | tee /data/web_static/releases/test/index.html > /dev/null 2>&1
+ubuntu@89-web-01:~/$ curl localhost/hbnb_static/index.html
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html > /dev/null 2>&1
 
 # Symbolic link
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Ownership
-chown -R ubuntu:ubuntu /data/
+chown -hR ubuntu:ubuntu /data/
 
 # creating /hbnb_static location
 nginx_conf="/etc/nginx/sites-available/default"
@@ -55,3 +42,5 @@ if ! grep -qF "location /hbnb_static" "$nginx_conf"
 then
     sed -i "/error_page 404 \/404.html;/a $content_to_insert" "$nginx_conf"
 fi
+
+service nginx restart
