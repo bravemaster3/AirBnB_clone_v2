72102e605904485dc4771caacf58669f5d890483
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-01-11 19:55:44 +0000

    Add 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" Fabric script for creating archive from github repos"""
+from fabric.api import local, put, env, run, sudo
+from datetime import datetime
+import os
+env.hosts = [
+    '3.85.54.241',
+    '52.86.86.171'
+]
+
+
+def do_pack():
+    """creates a tgz archive from the current folder"""
+    local("mkdir -p versions")
+    out_name = f"web_static_{datetime.now().strftime('%Y%m%d%H%M%S')}.tgz"
+    result = local("tar -cvzf versions/{} web_static".format(out_name),
+                   capture=True)
+    if not result.return_code:
+        return f"versions/{out_name}"
+    return None
+
+
+def do_deploy(archive_path):
+    """distributes an archive to webservers"""
+    if not os.path.exists(archive_path):
+        return False
+
+    basename = os.path.basename(archive_path)
+    rem_archive_path = f"/tmp/{basename}"
+
+    try:
+        put(archive_path, rem_archive_path)
+        x_archive = "/data/web_static/releases/{}".format(
+            os.path.splitext(basename)[0]
+        )
+        run(f"mkdir -p {x_archive}")
+        run("tar -xvf {} -C {} --strip-components=1".format(
+            rem_archive_path, x_archive
+            ))
+        run(f"rm -f {rem_archive_path}")
+        symlink = "/data/web_static/current"
+        run(f"rm -rf {symlink}")
+        run(f"ln -sf {x_archive} {symlink}")
+    except Exception:
+        return False
+
+    return True
