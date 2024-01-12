2302dd9e2adef664e1e05c649f30095292a389a4
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-01-12 09:02:47 +0000

    Attemp to fix 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static.
 # Installation of nginx if not already installed
-apt-get update
-apt-get upgrade
-apt-get install -y nginx
+if ! command -v nginx &> /dev/null
+then
+    apt-get update
+    apt-get upgrade
+    apt-get install -y nginx
+fi
 #folder creations
 mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 echo "Hello Again World!" | tee /data/web_static/releases/test/index.html
@@ -14,10 +17,11 @@ chown -hR ubuntu:ubuntu /data/
 # creating /hbnb_static location
 nginx_conf="/etc/nginx/sites-available/default"
 content_to_insert="\\
+\\    location /hbnb_static {\n\talias /data/web_static/current/;\n\tindex index.html index.htm;\n    }\\
 \\
-\\    location /hbnb_static {\\n\\talias /data/web_static/current/;\\n\\tindex index.html index.htm;\\n    }"
+\\    error_page 404 \/404.html;"
 if ! grep -qF "location /hbnb_static" "$nginx_conf"
 then
-    sed -i "/error_page 404 \/404.html;/a $content_to_insert" "$nginx_conf"
+    sed -i "/error_page 404 \/404.html;/c $content_to_insert" "$nginx_conf"
 fi
 service nginx start
