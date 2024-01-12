7e365e74f0f7482ed6dde87c507b01c3ebb20706
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-01-12 18:52:47 +0000

    removed pack from 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,17 +9,6 @@ env.hosts = [
 ]
 
 
-def do_pack():
-    """creates a tgz archive from the current folder"""
-    local("mkdir -p versions")
-    out_name = f"web_static_{datetime.now().strftime('%Y%m%d%H%M%S')}.tgz"
-    result = local("tar -cvzf versions/{} web_static".format(out_name),
-                   capture=True)
-    if not result.return_code:
-        return f"versions/{out_name}"
-    return None
-
-
 def do_deploy(archive_path):
     """distributes an archive to webservers"""
     if not os.path.exists(archive_path):
